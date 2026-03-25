58127882ac33c4d11ebe13d132edc3f63f5a08af
coolstufflola <149669233+coolstufflola@users.noreply.github.com>
2026-03-26 10:24:50 +1100

    Fixed 2 scripts conflicting.

--- boss.lua
+++ boss.lua
@@ -1,15 +1,13 @@
-local loadstrings = {
-    'loadstring(game:HttpGet(('https://raw.githubusercontent.com/itsyouranya/free/main/Anya%20Stands%20Awakening%20Helper.lua'),true))()',
-    'loadstring(game:HttpGet("https://raw.githubusercontent.com/sunexn/standsawakening/main/uncanny.lua",true))()'
-} -- table closed properly
-
-for i = 1, 2 do
-    spawn(function()
-        local f, err = loadstring(loadstrings[i])
-        if not f then
-            warn("Compile error #" .. i .. ": " .. err)
-            return
-        end
-        pcall(f)
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/itsyouranya/free/main/Anya%20Stands%20Awakening%20Helper.lua", true))()
     end)
-end
+end)
+
+task.wait(1.5)
+
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/sunexn/standsawakening/main/uncanny.lua", true))()
+    end)
+end)
